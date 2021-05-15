3d8c4dbc8d9b90243d57d2c4a7ee1aa54ec67f42
Tyler Knapp <tylerknapp25@gnmail.com[D[D[D[D[D[D[D[[C[C[C[C[C[Cmail.com>
2021-05-15 15:43:51 -0400

    Progress on HW assignment

--- module-1/05_Command_Line_Programs/exercise-student/src/main/java/com/techelevator/Fibonacci.java
+++ module-1/05_Command_Line_Programs/exercise-student/src/main/java/com/techelevator/Fibonacci.java
@@ -1,8 +1,40 @@
 package com.techelevator;
 
+import java.util.Scanner;
+
 public class Fibonacci {
 
 	public static void main(String[] args) {
+
+		// Get user input
+		Scanner input = new Scanner(System.in);
+
+		System.out.print( " Please enter a number: " );
+		int userFibNumber = input.nextInt();
+		input.nextLine();
+
+		int fibPositionZero = 0;
+		int fibPositionOne = 1;
+		int fibSumPreviousTwoInts;
+		
+		if(userFibNumber == 0) {
+			System.out.println("0, 1");
+		} else if(userFibNumber == 1) {
+			System.out.println("0, 1, 1");
+		} else {
+			for(int i = 0; i <= fibPositionOne ; i++){
+				if( fibPositionZero <= userFibNumber) {
+					if(userFibNumber > 1){
+						System.out.print( fibPositionZero + " , ");
+					}
+				}
+				fibSumPreviousTwoInts = fibPositionZero + fibPositionOne;
+				fibPositionZero = fibPositionOne;
+				fibPositionOne = fibSumPreviousTwoInts;
+			}
+		}
+
+
 
 	}
 
