e0cbce3a69f7816113e5236b2451ab2fd3469999
Tyler Knapp <tylerknapp25@gnmail.com[D[D[D[D[D[D[D[[C[C[C[C[C[Cmail.com>
2021-05-16 09:23:26 -0400

    Progress on HW Assignment

--- module-1/05_Command_Line_Programs/exercise-student/src/main/java/com/techelevator/DecimalToBinary.java
+++ module-1/05_Command_Line_Programs/exercise-student/src/main/java/com/techelevator/DecimalToBinary.java
@@ -1,8 +1,21 @@
 package com.techelevator;
 
+import java.util.Scanner;
+
 public class DecimalToBinary {
 
 	public static void main(String[] args) {
+
+		//Get user input
+
+		Scanner input = new Scanner(System.in);
+
+		System.out.println("Please enter in a series of decimal values (separated by spaces): ");
+		//Convert String to an array?
+
+		double userInput = input.nextDouble();
+		input.nextLine();
+
 
 	}
 
