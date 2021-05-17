beebfade75518a6a86c5e3fa51571f50ccf23f3f
Tyler Knapp <tylerknapp25@gnmail.com[D[D[D[D[D[D[D[[C[C[C[C[C[Cmail.com>
2021-05-17 09:31:20 -0400

    Update to files

--- module-1/00a-week1-extra-exercises/src/main/java/com/techelevator/Exercises.java
+++ module-1/00a-week1-extra-exercises/src/main/java/com/techelevator/Exercises.java
@@ -8,8 +8,17 @@ public class Exercises {
 	 arrayCount9([1, 9, 9]) → 2
 	 arrayCount9([1, 9, 9, 3, 9]) → 3
 	 */
+
+
 	public int arrayCount9(int[] nums) {
-		return 0;
+
+		int count = 0;
+		for( int i = 0 ; i < nums.length ; i++){
+			if(nums[i] == 9){
+				count++;
+			}
+		}
+		return count;
 	}
 
 	/*
@@ -18,8 +27,17 @@ public class Exercises {
 	 arrayFront9([1, 2, 3, 4, 9]) → false
 	 arrayFront9([1, 2, 3, 4, 5]) → false
 	 */
+
 	public boolean arrayFront9(int[] nums) {
-		return false;
+
+		boolean isThereANineInTheFirstFourPositions = false;
+
+		for(int i = 0 ; i < nums.length  ; i++){
+			if( nums[i] == 9 && i < 4 ){
+				isThereANineInTheFirstFourPositions = true;
+			}
+		}
+		return isThereANineInTheFirstFourPositions;
 	}
 
 	/*
@@ -28,8 +46,15 @@ public class Exercises {
 	 array123([1, 1, 2, 4, 1]) → false
 	 array123([1, 1, 2, 1, 2, 3]) → true
 	 */
+
+
 	public boolean array123(int[] nums) {
-		return false;
+		boolean isThereAOneTwoThreeSequence = false;
+		for( int i = 0 ; i < nums.length - 2 ; i++ )
+			if ( nums[i] == 1 && nums[i+1] == 2 && nums[i+2] == 3){
+				isThereAOneTwoThreeSequence = true;
+			}
+		return isThereAOneTwoThreeSequence;
 	}
 	
 	/*
@@ -40,7 +65,13 @@ public class Exercises {
 	 noTriples([1, 1, 1, 2, 2, 2, 1]) → false
 	 */
 	public boolean noTriples(int[] nums) {
-		return false;
+		boolean areThereThreeOfTheSameValuesInARow = true;
+		for ( int i = 0 ; i < nums.length -2 ; i++){
+			if( nums[i] == nums[i+1] && nums[i+1] == nums[i+2] ){
+				areThereThreeOfTheSameValuesInARow = false;
+			}
+		}
+		return areThereThreeOfTheSameValuesInARow;
 	}
 
 	/*
@@ -51,7 +82,7 @@ public class Exercises {
 	 makeEnds([7, 4, 6, 2]) → [7, 2]
 	 */
 	public int[] makeEnds(int[] nums) {
-		return new int[]{};
+		return new int[]{nums[0] , nums[nums.length - 1]};
 	}
 
 	/*
@@ -61,6 +92,9 @@ public class Exercises {
 	 has23([4, 5]) → false
 	 */
 	public boolean has23(int[] nums) {
+		if( nums[0] == 2 || nums[0] == 3 || nums[1] == 2 || nums[1] == 3){
+			return true;
+		}
 		return false;
 	}
 
@@ -71,7 +105,10 @@ public class Exercises {
 	 no23([3, 5]) → false
 	 */
 	public boolean no23(int[] nums) {
-		return false;
+		if( nums[0] == 2 || nums[0] == 3 || nums[1] == 2 || nums[1] ==3 ){
+			return false;
+		}
+		return true;
 	}
 
 	/*
@@ -83,7 +120,13 @@ public class Exercises {
 	 makeLast([3]) → [0, 3]
 	 */
 	public int[] makeLast(int[] nums) {
-		return new int[]{};
+
+		int[] newArray = new int[nums.length * 2];
+
+		newArray[newArray.length -1] = nums[nums.length -1];
+
+		return newArray;
+
 	}
 
 	/*
@@ -93,6 +136,10 @@ public class Exercises {
 	 double23([2, 3]) → false
 	 */
 	public boolean double23(int[] nums) {
+
+		if( nums.length > 1){
+			return (nums[0] == 2 && nums[1] == 2) || (nums[0] == 3 && nums[1] == 3);
+		}
 		return false;
 	}
 
@@ -104,6 +151,17 @@ public class Exercises {
 	 fix23([1, 2, 1]) → [1, 2, 1]
 	 */
 	public int[] fix23(int[] nums) {
+
+		int count = 0;
+		boolean isThereATwoFollowedByAThree = false;
+		//newArray[] = new int[]
+
+		for( int i = 0 ; i < nums.length -1 ; i++){
+			if(nums[i] == 2 && nums[i+1] == 3){
+				//newArray =
+		}
+
+		}
 		return new int[]{};
 	}
 
