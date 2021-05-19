0a4bdd3e7a574bedef1e16f8b54aa211e443be3f
B L <blauvray@gmail.com>
2021-05-19 11:55:31 -0400

    Lecture code

--- module-1/08_Collections_Part_2/lecture-final/sets_and_maps/src/main/java/com/techelevator/Lecture.java
+++ module-1/08_Collections_Part_2/lecture-final/sets_and_maps/src/main/java/com/techelevator/Lecture.java
@@ -1,10 +1,7 @@
 package com.techelevator;
 
-import java.util.HashMap;
-import java.util.LinkedHashMap;
+import java.util.*;
 import java.util.Map;
-import java.util.Set;
-import java.util.TreeMap;
 
 public class Lecture {
 
@@ -19,17 +16,47 @@ public class Lecture {
 
 		/* DECLARE AND INSTANTIATE A SET */
 
-		
-		/* ADD ITEMS TO THE SET */
+		// HashSet is the most commonly used.  It does not maintain order.
+		Set<Integer> setOfNumbers = new HashSet<Integer>();
 
+		// LinkedHashSet maintains the order of insertion
+		//Set<Integer> setOfNumbers = new LinkedHashSet<Integer>();
+
+		// TreeSet does not allow null and maintains the natural order of the data type
+		//Set<Integer> setOfNumbers = new TreeSet<Integer>();
+
+		/* ADD ITEMS TO THE SET */
+		setOfNumbers.add(1);
+		setOfNumbers.add(10);
+		setOfNumbers.add(30);
+		setOfNumbers.add(301);
+		/* duplicates are ignored */
+		setOfNumbers.add(301);
+		setOfNumbers.add(301);
+		setOfNumbers.add(42);
+		setOfNumbers.add(5708);
 		
 		/* LOOP OVER A SET */
 
+		for (Integer number : setOfNumbers) {
+			System.out.println(number);
+		}
 
 		
 		/*USE CASE:  USE A SET TO REMOVE DUPLICATES AND ORDER THE ARRAY */
 		String[] instructorWorkLog = {"Rachelle", "John", "Matt", "Kevin", "Rachelle", "Steve", "John", "Rachelle" };
-		
+
+		Set<String> worklogSet = new TreeSet<String>();
+
+		// Add it to the set to remove duplicates
+		for (String instructors : instructorWorkLog) {
+			worklogSet.add(instructors);
+		}
+
+		// just here to print out the results
+		for (String instructor: worklogSet) {
+			System.out.println(instructor);
+		}
 
 		
 		
@@ -39,35 +66,61 @@ public class Lecture {
 		System.out.println();
 		
 		/* DECLARING AND INSTANTIATING A MAP */
-
+		Map<String, String> animalNoises = new HashMap<String, String>();
 		
 		/* ADDING ITEMS TO A MAP */
-
+		animalNoises.put("Cow", "Moo");
+		animalNoises.put("Chicken", "Cluck");
+		animalNoises.put("Dog", "Bark");
+		animalNoises.put("Cat", "Meow");
+		animalNoises.put("Lion", "Roar");
+		animalNoises.put("Duck", "Roar");
 		
 		/* UPDATING AN ITEM IN A MAP */
-
+		// if put with an existing key then the existing value is replaced with the new value
+		animalNoises.put("Duck", "Quack");
 
 		/* RETRIEVING AN ITEM FROM A MAP */
+		String chickenSound = animalNoises.get("Chicken");
+		System.out.println(chickenSound);
 
-			
+		// If the key does not exist in the map, then get() returns null
+		String foxSound = animalNoises.get("fox");
+
+
 		/* REMOVING AN ITEM FROM A MAP */
-		
-		// If the key does not exists, the null returned
+		String lionNoise = animalNoises.remove("Lion");
 
+		// If the key does not exists, the null returned
+		String removeFoxReturn = animalNoises.remove("fox");
 		
 		
 		/* CHECK IF AN ITEM EXISTS */
 		// containsKey(key) returns TRUE if the KEY exists in the Map
+		boolean catExists = animalNoises.containsKey("Cat");
+		boolean mooExistsAsKey = animalNoises.containsKey("Moo");
 
 		// containsValue(value) returns TRUE if the VALUE exists in the Map
+		boolean mooExists = animalNoises.containsValue("Moo");
+		boolean catExistsAsValue = animalNoises.containsValue("Cat");
 
-		
 		System.out.println();
 		
 		
 		/* LOOPING OVER A MAP */
-		// Loop through a map by looping through the Keys
+		// Loop through a map by looping through the Keys (using keySet())
 		// Then using the keys to get the value
+
+		for (String animalName : animalNoises.keySet()) {
+			// For each key can use it to get the value
+			String animalNoise = animalNoises.get(animalName);
+			System.out.println("The " + animalName + " says " + animalNoise);
+		}
+
+		// Loop through the map using the EntrySet
+		for(Map.Entry<String, String> mapEntry : animalNoises.entrySet()) {
+			System.out.println("The " + mapEntry.getKey() + " says " + mapEntry.getValue());
+		}
 
 
 		System.out.println();
@@ -79,7 +132,11 @@ public class Lecture {
 		accounts.put(56789, 200d);
 		
 		// Transfer half of Map 12345's money to account 56789
-		
+		double halfOfAccount12345 = accounts.get(12345) / 2.0;
+		accounts.put(12345, accounts.get(12345) - halfOfAccount12345);
+		accounts.put(56789, accounts.get(56789) + halfOfAccount12345);
+
+
 
 		
 		
