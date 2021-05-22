897691d1bdf2d34564d4e16e2fd7524a56935889
Tyler Knapp <tylerknapp25@gnmail.com[D[D[D[D[D[D[D[[C[C[C[C[C[Cmail.com>
2021-05-22 16:47:53 -0400

    Update

--- module-1/09_Classes_Encapsulation/exercise-student/src/main/java/com/techelevator/Elevator.java
+++ module-1/09_Classes_Encapsulation/exercise-student/src/main/java/com/techelevator/Elevator.java
@@ -2,4 +2,30 @@ package com.techelevator;
 
 public class Elevator {
 
+    private int currentFloor = 1;
+    private int numberOfFloors;
+    private boolean doorOpen;
+
+    public Elevator(int numberOfLevels){
+        this.numberOfFloors = numberOfLevels;
+    }
+
+    //must be closed to open.
+    public void openDoor(){
+        if(isDoorOpen()){
+            doorOpen = isDoorOpen();
+        }
+    }
+
+    public int getCurrentFloor() {
+        return this.currentFloor;
+    }
+
+    public int getNumberOfFloors() {
+        return this.numberOfFloors;
+    }
+
+    public boolean isDoorOpen() {
+        return this.doorOpen;
+    }
 }
