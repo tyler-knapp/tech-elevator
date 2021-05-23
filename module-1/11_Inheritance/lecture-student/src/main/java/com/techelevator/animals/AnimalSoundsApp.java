c0b2eb6abf59227ca05954b0cfbea229cae6b147
Brian Lauvray <brian@techelevator.com>
2021-05-23 16:20:52 -0400

    Added Inheritance

---
+++ module-1/11_Inheritance/lecture-student/src/main/java/com/techelevator/animals/AnimalSoundsApp.java
@@ -0,0 +1,12 @@
+package com.techelevator.animals;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AnimalSoundsApp {
+
+    public static void main(String[] args) {
+
+
+    }
+}
---
+++ module-1/11_Inheritance/lecture-student/src/main/java/com/techelevator/calculator/CalculatorApp.java
@@ -0,0 +1,13 @@
+package com.techelevator.calculator;
+
+public class CalculatorApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+
+		
+	}
+	
+
+}
