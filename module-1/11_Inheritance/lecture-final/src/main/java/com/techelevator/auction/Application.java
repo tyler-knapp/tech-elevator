2285428c84f745e788a2a14ce786330ecc1d47dc
B L <blauvray@gmail.com>
2021-05-24 16:02:44 -0400

    lecture code

--- module-1/11_Inheritance/lecture-final/src/main/java/com/techelevator/auction/Application.java
+++ module-1/11_Inheritance/lecture-final/src/main/java/com/techelevator/auction/Application.java
@@ -1,5 +1,8 @@
 package com.techelevator.auction;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
 
     public static void main(String[] args) {
@@ -10,12 +13,32 @@ public class Application {
 
         Auction generalAuction = new Auction("Tech Elevator t-shirt");
 
-        generalAuction.placeBid(new Bid("Josh", 1));
-        generalAuction.placeBid(new Bid("Fonz", 23));
-        generalAuction.placeBid(new Bid("Jenny", 13));
+        System.out.println(generalAuction.placeBid(new Bid("Josh", 1)));
+        System.out.println(generalAuction.placeBid(new Bid("Fonz", 23)));
+        System.out.println(generalAuction.placeBid(new Bid("Jenny", 13)));
         //....
         //....
         // This might go on until the auction runs out of time or hits a max # of bids
 
+        BuyoutAuction buyoutAuction = new BuyoutAuction("Book", 20);
+        buyoutAuction.placeBid( new Bid("name", 22));
+
+        ReserveAuction reserveAuction = new ReserveAuction("Pen", 5);
+        reserveAuction.placeBid(new Bid("losing", 2));
+        reserveAuction.placeBid(new Bid("winning", 10));
+
+
+        /*
+        Polymorphism below this
+         */
+        List<Auction> auctions = new ArrayList<Auction>();
+
+        auctions.add(generalAuction);
+        auctions.add(buyoutAuction);
+        auctions.add(reserveAuction);
+
+        for (Auction auction: auctions) {
+            System.out.println( auction.placeBid( new Bid("Polymorphism", 4)));
+        }
     }
 }
--- module-1/11_Inheritance/lecture-final/src/main/java/com/techelevator/calculator/CalculatorApp.java
+++ module-1/11_Inheritance/lecture-final/src/main/java/com/techelevator/calculator/CalculatorApp.java
@@ -5,6 +5,11 @@ public class CalculatorApp {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		Calculator calculator = new Calculator(0);
+
+		ScientificCalculator sciCalculator = new ScientificCalculator(0);
+
+		TrignometricCalculator trigCalculator = new TrignometricCalculator();
 
 		
 	}
