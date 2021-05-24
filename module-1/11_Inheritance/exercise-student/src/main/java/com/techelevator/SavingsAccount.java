38f9854f59c00bfc8f002cba3d5b4207ee6625d0
Tyler Knapp <tylerknapp25@gnmail.com[D[D[D[D[D[D[D[[C[C[C[C[C[Cmail.com>
2021-05-24 18:00:08 -0400

    HW Submission

--- module-1/11_Inheritance/exercise-student/src/main/java/com/techelevator/SavingsAccount.java
+++ module-1/11_Inheritance/exercise-student/src/main/java/com/techelevator/SavingsAccount.java
@@ -2,20 +2,20 @@ package com.techelevator;
 
 public class SavingsAccount extends BankAccount{
 
-    public SavingsAccount(String accountHoldersName, String accountNumber, int balance){
-        super(accountHoldersName,accountNumber,balance);
+    public SavingsAccount( String accountHoldersName, String accountNumber, int balance ){
+        super( accountHoldersName, accountNumber, balance );
     }
 
-    public SavingsAccount(String accountHoldersName, String accountNumber){
-        super(accountHoldersName, accountNumber);
+    public SavingsAccount( String accountHoldersName, String accountNumber ){
+        super( accountHoldersName, accountNumber );
     }
 
     @Override
     public int withdraw(int amountToWithdraw) {
 
-        if(super.getBalance() > 150) {
+        if(super.getBalance() - amountToWithdraw >= 150) {
             super.withdraw(amountToWithdraw);
-        }else if( super.getBalance() - amountToWithdraw < 150  && super.getBalance()  >= 0 ) {
+        }else if( super.getBalance() - amountToWithdraw < 150  && super.getBalance() >= 0 ) {
                 super.withdraw(amountToWithdraw + 2);
             } else {
                 return super.getBalance();
