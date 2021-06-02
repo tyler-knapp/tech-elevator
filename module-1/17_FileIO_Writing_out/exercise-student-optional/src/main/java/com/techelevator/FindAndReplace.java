28aaf8517ca4bb90c32316ddaa862483b5b123b1
Tyler Knapp <tylerknapp25@gnmail.com[D[D[D[D[D[D[D[[C[C[C[C[C[Cmail.com>
2021-06-02 14:55:29 -0400

    Update to files

--- module-1/17_FileIO_Writing_out/exercise-student-optional/src/main/java/com/techelevator/FindAndReplace.java
+++ module-1/17_FileIO_Writing_out/exercise-student-optional/src/main/java/com/techelevator/FindAndReplace.java
@@ -1,8 +1,43 @@
 package com.techelevator;
 
+import java.io.File;
+import java.util.Scanner;
+
 public class FindAndReplace {
 
     public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        String path = "C:\\Users\\Student\\source\\repos\\tylerknapp-java-blue\\module-1\\17_FileIO_Writing_out\\exercise-student-optional\\src\\test\\resources\\bacon.txt";
+
+        System.out.println("What is the search word?");
+        String searchWord = input.nextLine();
+
+        System.out.println("What is the replacement word?");
+        String replacementWord = input.nextLine();
+
+        System.out.println("What is the source file?");
+        String pathToSourceFile = input.nextLine();
+
+        System.out.println("What is the destination file?");
+        String destinationFilePath = input.nextLine();
+
+        //C:\Users\Student\source\repos\tylerknapp-java-blue\module-1\17_FileIO_Writing_out\exercise-student-optional\src\test\resources\bacon.txt
+
+        File file = new File( pathToSourceFile );
+
+        if(!file.exists()) {
+            System.out.println("File does not exist");
+        }
+        if(!file.isFile()) {
+            System.out.println("This is not a file");
+        }
+
+
+
+
+
 
     }
 
