91b5abe99d5132d762acc0b4122a33b6b835324f
Tyler Knapp <tylerknapp25@gnmail.com[D[D[D[D[D[D[D[[C[C[C[C[C[Cmail.com>
2021-06-03 09:16:32 -0400

    Homework Submission

--- module-1/17_FileIO_Writing_out/exercise-student-optional/src/main/java/com/techelevator/FindAndReplace.java
+++ module-1/17_FileIO_Writing_out/exercise-student-optional/src/main/java/com/techelevator/FindAndReplace.java
@@ -55,7 +55,5 @@ public class FindAndReplace {
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
-
     }
-
 }
--- module-1/17_FileIO_Writing_out/exercise-student-optional/src/main/java/com/techelevator/FizzWriter.java
+++ module-1/17_FileIO_Writing_out/exercise-student-optional/src/main/java/com/techelevator/FizzWriter.java
@@ -1,9 +1,36 @@
 package com.techelevator;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
 public class FizzWriter {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws FileNotFoundException {
+
+		Scanner input = new Scanner(System.in);
+
+		System.out.println("What is the destination file?");
+		String destinationFilePath = input.nextLine();
+
+		File destFile = new File(destinationFilePath);
+
+		String output;
+		try (PrintWriter dataOutput = new PrintWriter(destFile)) {
+			for( int i = 1; i <= 300; i++){
+				if(i % 15 == 0 ){
+					output = "FizzBuzz";
+				} else if ( i % 3 == 0 || Integer.toString(i).contains("3")){
+					output = "Fizz";
+				} else if (i % 5 ==0 || Integer.toString(i).contains("5")){
+					output ="Buzz";
+				} else {
+					output = Integer.toString(i);
+				}
+				dataOutput.println(output);
+			}
+		}
 
 	}
-
 }
