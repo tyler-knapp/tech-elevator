12acbbdd23c2b62d9e869d5b6336a7a4a3176812
Tyler Knapp <tylerknapp25@gnmail.com[D[D[D[D[D[D[D[[C[C[C[C[C[Cmail.com>
2021-06-07 23:06:45 -0400

    Update to Database HW

--- module-2/01_Introduction_to_Databases/exercise-student/intro-to-databases-exercises.sql
+++ module-2/01_Introduction_to_Databases/exercise-student/intro-to-databases-exercises.sql
@@ -5,32 +5,84 @@
 --   The value immediately after the problem statement is the expected number of rows that should be returned by the query.
 
 -- 1. The name and population of all cities in Ontario, Canada (27 rows)
+SELECT name, population
+FROM city
+WHERE countrycode = 'CAN'
+AND district = 'Ontario';
 
 -- 2. The name and population of all cities in Montana (1 row)
+SELECT name, population
+FROM city
+WHERE countrycode = 'USA'
+AND district = 'Montana';
 
 -- 3. The name, form of government, and head of state of all countries in Europe (46 rows)
 
+SELECT name, governmentform, headofstate
+FROM country
+WHERE continent = 'Europe';
+
 -- 4. The name, population, surface area, and average life expectancy of all countries in Asia (51 rows)
+SELECT name, population, surfacearea, lifeexpectancy
+FROM country
+WHERE continent = 'Asia';
 
 -- 5. The name, country code, and population of all cities with a population greater than 8 million people (10 rows)
+SELECT name, countrycode, population
+FROM city
+WHERE population > 8000000;
 
 -- 6. The name, country code, and population of all cities with a population less than one thousand people (11 rows)
+SELECT name, countrycode, population
+FROM city
+WHERE population < 1000;
 
 -- 7. The name, continent, and GNP of all countries with a GNP greater than one trillion dollars (6 rows)
+SELECT name, continent, gnp
+FROM country
+WHERE gnp > 1000000;
 
 -- 8. The name, continent, population, GNP, and average life expectancy of all countries with an average life expectancy greater than 80 years (5 rows)
+SELECT name, continent, population, gnp, lifeexpectancy
+FROM country   
+WHERE lifeexpectancy > 80;
 
 -- 9. The name and population of all cities in the USA with a population of greater than 1 million people (9 rows)
+SELECT name, population
+FROM city
+WHERE countrycode = 'USA'
+AND population > 1000000;
 
 -- 10. The name and population of all cities in China with a population of greater than 1 million people (35 rows)
+SELECT name, population
+FROM city
+WHERE countrycode = 'CHN'
+AND population > 1000000;
 
 -- 11. The name and region of all countries in North or South America (51 rows)
+SELECT name, region
+FROM country
+WHERE continent = 'North America'
+OR continent ='South America';
+-- Why would this be an OR here and not an AND? I get that's what the question is asking for
+--but it seems like using AND should comine both as well?
 
 -- 12. The name, continent, and head of state of all countries whose form of government is a monarchy (43 rows)
+SELECT name, continent, headofstate
+FROM country
+WHERE governmentform LIKE '%Monarchy%';
 
 -- 13. The name of all cities in the USA with a population between 1 million and 2 million people (6 rows) 
+SELECT name
+FROM city
+WHERE countrycode = 'USA'
+AND population BETWEEN 1000000 AND 2000000;
 
 -- 14. The name and region of all countries in North or South America except for countries in the Caribbean (27 rows)
+SELECT *
+FROM country
+WHERE continent = 'North America'
+OR continent = 'South America';
 
 -- 15. The name, population, and GNP of all countries with a GNP greater than $1 trillion dollars and a population of less than 100 million people (4 rows)
 
--- module-2/01_Introduction_to_Databases/lecture-student/lecture-queries.sql
+++ module-2/01_Introduction_to_Databases/lecture-student/lecture-queries.sql
@@ -1,43 +1,102 @@
 -- SELECT ... FROM
 -- Selecting the names for all countries
-
+SELECT name                --SELECT columns filters the columns to be returned
+FROM country;           -- FROM is the table to get the data from
 
 -- Selecting the name and population of all countries
-
+SELECT   name, population
+FROM country;
 
 -- Selecting all columns from the city table
+SELECT *                --* returns every column
+FROM city;
 
+SELECT DISTINCT district        --DISTINCT with a column will only return the unique vales from that colum
+FROM city;
+
+SELECT name AS city_name -- AS can alias a column so it has a different name in the results
+FROM city;
 
 -- SELECT ... FROM ... WHERE
 -- Selecting the cities in Ohio
-
+SELECT * 
+FROM city
+WHERE district = 'Ohio';
 
 -- Selecting countries that gained independence in the year 1776
-
+SELECT *
+FROM country
+WHERE indepyear = 1776;
 
 -- Selecting countries not in Asia
-
+SELECT *
+FROM country
+WHERE continent != 'Asia';
 
 -- Selecting countries that do not have an independence year
+SELECT *
+FROM country
+WHERE indepyear IS NULL;
 
 -- Selecting countries that do have an independence year
-
+SELECT *
+FROM country
+WHERE indepyear IS NOT NULL;
 
 -- Selecting countries that have a population greater than 5 million
-
+SELECT *
+FROM country
+WHERE population > 5000000;
 
 
 -- SELECT ... FROM ... WHERE ... AND/OR
 -- Selecting cities in Ohio and Population greater than 400,000
+SELECT *
+FROM city
+WHERE district = 'Ohio'
+AND population > 400000;
 
 -- Selecting country names on the continent North America or South America
 
+SELECT *
+FROM country
+WHERE continent = 'North America'
+OR continent = 'South America';
 
+--The above query could also be written as
+SELECT name
+FROM country
+WHERE continent IN ('North America' , 'South America');
 
+--Between allows a selection between two values 
+SELECT *
+FROM country
+WHERE population BETWEEN 1000000 AND 2000000;
+
+--Select all countries that begin with the letter A
+SELECT *
+FROM country
+WHERE name LIKE 'A%';
+
+--Select all countries that end with the letter d
+SELECT *
+FROM country
+WHERE name Like '%d';
+
+--Select all countries that contain la
+SELECT *
+FROM country
+WHERE name LIKE '%la%';
+
+--LIKE is case sensitive, ILIKE is not case sensitive (ILIKE is Postgres only)
+SELECT *
+FROM country 
+WHERE name ILIKE '%d%';
 
 -- SELECTING DATA w/arithmetic
 -- Selecting the population, life expectancy, and population per area
 --	note the use of the 'as' keyword
-
+SELECT population, lifeexpectancy, (population / surfacearea) AS population_density
+FROM country;
 
 
